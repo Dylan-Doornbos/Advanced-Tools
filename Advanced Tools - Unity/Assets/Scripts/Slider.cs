be4a6a9bcb2f50d2b0066a2560a62fce932bcf48
Dylan <dylan_d@hotmail.nl>
2022-04-13 11:33:32 +0200

    Added/modified meshes and settings for additional testing.

---
+++ Advanced Tools - Unity/Assets/Scripts/Slider.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slider : MonoBehaviour
+{
+    private UnityEngine.UI.Slider _slider;
+    [SerializeField] private Text _text;
+    [SerializeField] private int _increments;
+
+    private void Awake()
+    {
+        _slider = GetComponent<UnityEngine.UI.Slider>();
+    }
+
+    public void OnValueChanged()
+    {
+        _slider.value = Mathf.CeilToInt(_slider.value / _increments) * _increments;
+    }
+
+    public void SetText()
+    {
+        _text.text = _slider.value.ToString();
+    }
+}
